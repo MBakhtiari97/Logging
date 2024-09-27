876e64e93197a26a608211bdd3a5b77180b6a44c
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2024-09-27 17:06:59 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Logging;
+internal partial class Program
+{
+    static void Main(string[] args)
+    {
+        using ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+        ILogger logger = factory.CreateLogger("Program");
+        //Console.WriteLine("Hello, World!");
+        //LogStartupMessage(logger, "fun");
+        logger.LogCritical("this is a log");
+    }
+
+    [LoggerMessage(Level = LogLevel.Information, Message = "Hello World! Logging is {Description}.")]
+    static partial void LogStartupMessage(ILogger logger, string description);
+}
